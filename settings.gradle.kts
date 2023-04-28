1dd97646a1a3320d936ad1588123819eaaa6e6b9
SorrowBlue <sorrowblue.dev@gmail.com>
2023-04-28 23:58:07 +0900

    :tada: Basic Views Activityのテンプレートをkotlin-dsl化する。

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.4.0")
+    id("com.android.settings") version "8.0.0"
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+extensions.configure<com.android.build.api.dsl.SettingsExtension> {
+    buildToolsVersion = "33.0.2"
+    compileSdk = 33
+    minSdk = 28
+}
+
+rootProject.name = "AndroidPlayGround"
+include(":app")
